3b103d0c631217dc9e78321f772ef0fcb6b0e42b
AdamNB-sys <a1adnat@gmail.com>
2022-03-24 23:17:26 -0500

    task 3, json format

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+"""Uses REST API to find an employee ID and return
+information about their todo list progress"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    """checks for valid input and returns employee info"""
+    if sys.argv[1].isdigit():
+        u_id = sys.argv[1]
+        response = requests.get(
+            'https://jsonplaceholder.typicode.com/todos/?userId={}'
+            .format(u_id))
+        users = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(u_id))
+
+        finished_tasks = []
+        todo_list = response.json()
+        employees = users.json()
+
+        for task in todo_list:
+            if task.get('completed') is True:
+                finished_tasks.append(task.get("title"))
+
+        emp_name = employees.get('name')
+        done = len(finished_tasks)
+        total = len(todo_list)
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            emp_name, done, total))
+        for title in finished_tasks:
+            print("\t {}".format(title))
+
+        info_json = []
+        for items in todo_list:
+            new_dict = {
+                'task': items.get('title'),
+                'completed': items.get('completed'),
+                'username': employees.get('username')
+            }
+            info_json.append(new_dict)
+        json_dict = {u_id: info_json}
+        out_file = open("{}.json".format(sys.argv[1]), "w")
+        json.dump(json_dict, out_file)
+        out_file.close()
