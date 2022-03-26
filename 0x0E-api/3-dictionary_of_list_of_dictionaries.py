7f374efb85a1d4449ad0d7ce2374d5b8bc16a4de
AdamNB-sys <a1adnat@gmail.com>
2022-03-26 17:34:17 -0500

    i think this is task 4 done baby

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,33 +13,18 @@ if __name__ == '__main__':
     todo_list = response.json()
     employees = users.json()
 
-    user_dict = {}
-    for item in employees:
-        user_dict[item.get('id')] = item.get('username')
     master_dict = {}
     for user in employees:
         id = user['id']
         master_dict[id] = []
-        # print(master_dict)
         for task in todo_list:
             if task['userId'] == id:
-                # print(task['userId'])
                 new_dict = {
+                    'username': user['username'],
                     'task': task.get('title'),
-                    'completed': task.get('completed'),
-                    'username': item['username']
+                    'completed': task.get('completed')
                     }
-                print(new_dict)
-    # info_json = []
-    # for items in todo_list:
-    #     new_dict = {
-    #         'task': items.get('title'),
-    #         'completed': items.get('completed'),
-    #         'username': employees.get('username')
-    #     }
-    #     info_json.append(new_dict)
-    #     employee_id = str(items.get('userId'))
-    # json_dict = {employee_id: info_json}
-    # out_file = open("todo_all_employees.json", "w")
-    # json.dump(json_dict, out_file)
-    # out_file.close()
+                master_dict[id].append(new_dict)
+        out_file = open("todo_all_employees.json", "w")
+        json.dump(master_dict, out_file)
+        out_file.close()
